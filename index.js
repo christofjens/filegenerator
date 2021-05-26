1d322c109c7582c264e6787b1330958b85a079b3
Christof Jens <christofjens@gmail.com>
2021-05-26 14:03:41 +0200

    Finalize step four

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const writeFile = require('./writeFile')
-const readline = require('readline')
+const inquirer = require('inquirer')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+const questions = [
+  {
+    type: 'input',
+    name: 'file',
+    message: 'Which functions and files would you like to create?\n',
+  },
+]
+
+inquirer.prompt(questions).then(answers => {
+  const files = answers['file'].split(',').map(el => el.trim())
+  files.forEach(file => writeFile(file))
 })
 
-rl.question(
-  'Which functions and files would you like to create? Please separate by comma. ',
-  answer => {
-    answer.split(',').map(name => writeFile(name.trim()))
+// rl.question(
+//   'Which functions and files would you like to create? Please separate by comma. ',
+//   answer => {
+//     answer.split(',').map(name => writeFile(name.trim()))
 
-    // for each word a new file, separate by comma
-    // writeFile([answer])
-    console.log([answer])
+//     // for each word a new file, separate by comma
+//     // writeFile([answer])
+//     console.log([answer])
 
-    rl.close()
-  }
-)
+//     rl.close()
+//   }
+// )
